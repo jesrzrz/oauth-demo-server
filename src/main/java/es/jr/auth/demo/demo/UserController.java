4e8893ed5335a45455668f7dc4e74041e045e632
a <a@a.com>
2019-09-16 23:30:42 +0200

    initial commit

---
+++ src/main/java/es/jr/auth/demo/demo/UserController.java
@@ -0,0 +1,16 @@
+package es.jr.auth.demo.demo;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@GetMapping("/user/me")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+	
+}
